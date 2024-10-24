7557bbbafe4c1efc4de1f277a100263bd9b6f851
sunshinehci <sunshinehcxiong@gmail.com>
2024-10-24 12:54:51 -0700

    Formatted Google Calendar

--- myapp/views.py
+++ myapp/views.py
@@ -8,3 +8,16 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+def googleCalendar(request):
+    iframe_code = '''
+    <iframe
+        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&bgcolor=%239E69AF&src=YzI0ZTliYzcwNDhkMzg0NDczMmM2NTY5NzljODY4MDgzNjZlOWI1MGVhZGM2ZmUxNTBjODY2OWE3YTYxMjRkMUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23B39DDB"
+        style="border:solid 1px #777"
+        width="500"
+        height="800"
+        frameborder="0"
+        scrolling="no">
+    </iframe>
+    '''
+    return render(request, 'googleCalendar.html', {'iframe_code': iframe_code})
