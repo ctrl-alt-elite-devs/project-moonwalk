3da7aa282475ed8d8c282e60acd8f7864aeef5e1
4runner99 <julian7005@gmail.com>
2024-10-27 16:42:29 -0700

    added url path

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/',views.contact, name='contact'),
-    path('cart/',views.cart, name='cart') 
+    path('cart/',views.cart, name='cart'),
     path('contact/', views.contact, name='contact'),
     path('googleCalendar/', views.googleCalendar, name='googleCalendar'),
 ]
