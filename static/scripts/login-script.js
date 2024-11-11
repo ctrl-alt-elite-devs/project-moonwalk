840b0f08a51dabeb2cda9bb040f1131d0012d5c6
Paul Arnett <paull.arnett@gmail.com>
2024-11-11 12:44:33 -0800

    Jira #196,197, 200, 204
    Added Account creation form, hooks of the entries, terms of service, and a google login button on login popup.

--- static/scripts/login-script.js
+++ static/scripts/login-script.js
@@ -3,14 +3,17 @@
 // Get the login and signup modals
 var loginModal = document.getElementById("login-modal");
 var signupModal = document.getElementById("signup-modal");
+var termsModal = document.getElementById("terms-modal");
 
 // Get the elements that open the modals
 var openLogin = document.querySelectorAll('.open_login');
 var openSignup = document.querySelectorAll('.open_signup');
+var openTerms = document.querySelectorAll(".terms-link");
 
 // Get the <span> elements that close the modals
 var closeLogin = document.getElementsByClassName("login-close")[0];
 var closeSignup = document.getElementsByClassName("signup-close")[0];
+var closeTerms = document.getElementsByClassName("terms-close")[0];
 
 // Add click event listeners to each button that opens the login modal
 openLogin.forEach(function(button) {
@@ -27,6 +30,13 @@ openSignup.forEach(function(button) {
   };
 });
 
+// Add click event listeners to each button that opens the terms modal
+openTerms.forEach(function(button) {
+  button.onclick = function() {
+    termsModal.style.display = "flex";
+  };
+});
+
 // Close login modal when clicking the close button
 closeLogin.onclick = function() {
   loginModal.style.display = "none";
@@ -35,6 +45,11 @@ closeLogin.onclick = function() {
 // Close signup modal when clicking the close button
 closeSignup.onclick = function() {
   signupModal.style.display = "none";
+}
+
+// Close signup modal when clicking the close button
+closeTerms.onclick = function() {
+  termsModal.style.display = "none";
 }
 
 // Close any modal when clicking outside of it
@@ -58,8 +73,10 @@ document.getElementById("signup-form").onsubmit = function(event) {
         "\nFirst Name: " + document.getElementById("first-name").value + 
         "\nLast Name: " + document.getElementById("last-name").value + 
         "\nPassword: " + document.getElementById("signup-password").value +
-        "\nConfirm Password: " + document.getElementById("confirm-password").value +
-        "\nPhone #: " + document.getElementById("signup-phone").value);
+        "\nPhone #: " + document.getElementById("signup-phone").value +
+        "\nText: " + (document.getElementById("text-checkbox").checked ? "Yes" : "No") +
+        "\nEmail: " + (document.getElementById("email-checkbox").checked ? "Yes" : "No")+
+        "\nTerms:" + (document.getElementById("agreement-checkbox").checked ? "Yes" : "No"));
   signupModal.style.display = "none";
 };
 
