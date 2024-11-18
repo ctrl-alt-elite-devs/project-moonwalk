e253ab05003240d911c9749beea92de41706c972
sunshinehci <sunshinehcxiong@gmail.com>
2024-11-17 17:37:44 -0800

    Checkout Page
    created checkout page that includes delivery options: delivery or store pickup.

--- myapp/views.py
+++ myapp/views.py
@@ -74,6 +74,9 @@ def googleCalendar(request):
     '''
     return render(request, 'googleCalendar.html', {'iframe_code': iframe_code})
 
+def checkout(request):
+    return render(request, 'checkout.html')
+
 #testing square api's
 def listLocations(request):
     if request.method == "GET":
