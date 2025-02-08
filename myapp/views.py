413062a9ff1bf90979448f1259f79164141db166
ayemyatnk <ayemyatnoekhin37@gmail.com>
2025-02-07 17:42:04 -0800

    Fetched event name and time dynamically from the calendar

--- myapp/views.py
+++ myapp/views.py
@@ -144,7 +144,7 @@ def merge_cart(sender, request, user, **kwargs):
 
 def googleCalendar(request):
     iframe_code = '''
-    <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&showPrint=0&showTz=0&showTabs=0&showTitle=0&src=YzI0ZTliYzcwNDhkMzg0NDczMmM2NTY5NzljODY4MDgzNjZlOWI1MGVhZGM2ZmUxNTBjODY2OWE3YTYxMjRkMUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23B39DDB" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    <iframe src="https://calendar.google.com/calendar/embed?src=900fdf650b579a007feb6e7f8ac879190337d00eac8e62aeffbbb6cd9ff10df3%40group.calendar.google.com&ctz=America%2FLos_Angeles" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
     '''
     return render(request, 'googleCalendar.html', {'iframe_code': iframe_code})
 
