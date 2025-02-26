978d02ee7dc31083ca97a16bfd59148cf60fb2b3
tjudtmartine <145167310+tjudtmartine@users.noreply.github.com>
2025-02-25 18:29:39 -0800

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -43,17 +43,6 @@ class Subscriber(models.Model):
         return self.email
     
 
-class Customer(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    phone = models.CharField(max_length=10)
-    email = models.EmailField(max_length=100)
-    password = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
-
-
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price= models.DecimalField(default=0, decimal_places=2, max_digits=6)
