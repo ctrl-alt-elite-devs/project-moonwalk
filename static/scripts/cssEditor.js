542e6bca9dace90cdbc7acbb4688142e2197ab23
ValiantPhoenix13 <axelkh2001@hotmail.com>
2025-03-10 18:43:00 -0700

    Implemented uploading images to the CSS editor.

--- static/scripts/cssEditor.js
+++ static/scripts/cssEditor.js
@@ -21,7 +21,29 @@ var titleWeight = document.querySelectorAll('input[name="weight"]');
 var fontColor = document.getElementById('color_font');
 var borderThickness = document.getElementById('border');
 var borderColor = document.getElementById('color_border');
+////////////////////////////////////////////////////////////////////////////////
 
+////////////////////////////////////////////////////////////////////////////////
+// Image Banner
+var compositionChoices = document.querySelectorAll('input[name="composition"]');
+var image00 = document.getElementById("image00");
+var image01 = document.getElementById("image01");
+var image02 = document.getElementById("image02");
+var maxwidth00 = 430; // Layout 1 (default)
+var maxheight00 = 331; // Layout 1 (default)
+var maxwidth01 = 430; // Layout 1 (default)
+var maxheight01 = 331; // Layout 1 (default)
+var maxwidth02 = 430; // Layout 1 (default)
+var maxheight02 = 331; // Layout 1 (default)
+var inputImg00 = document.getElementById("img00");
+var inputImg01 = document.getElementById("img01");
+var inputImg02 = document.getElementById("img02");
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// Submit button
+var submit = document.getElementById("done");
+////////////////////////////////////////////////////////////////////////////////
 
 console.log("cssEditor Loaded");
 
@@ -30,6 +52,51 @@ colorPicker.addEventListener("input", function(){
     colorValue.innerText = colorPicker.value; // Displays the hex of the current selected color
     editable.style.setProperty('--background_color', colorPicker.value);
 });
+
+// Method to change the composition of the images
+// May have to scrap image compositions
+compositionChoices.forEach((composition) => {
+    composition.addEventListener("click", function(){
+        if (composition.value == 'composition00'){
+            maxheight00 = 331
+            maxheight01 = 331; 
+            maxheight02 = 331;
+            maxwidth00 = 430;
+            maxwidth01 = 430; 
+            maxwidth02 = 430;
+        }
+        if (composition.value == 'composition01'){
+            maxwidth00 = 645;
+            maxwidth01 = 645;
+            maxwidth02 = 645;
+            maxheight00 = 331;
+            maxheight01 = 165;
+            maxheight02 = 165;
+            console.log(maxwidth00);
+        }
+        if (composition.value == 'composition02'){
+            
+        }
+    });
+});
+
+// Method to change displayed images
+inputImg00.onchange = function(){
+    image00.style.width = maxwidth00+'px';
+    image00.style.height = maxheight00+'px';
+    image00.style.backgroundSize = 100;
+    image00.src = URL.createObjectURL(inputImg00.files[0]);
+}
+inputImg01.onchange = function(){
+    image01.style.width = maxwidth01+'px';
+    image01.style.height = maxheight01+'px';
+    image01.src = URL.createObjectURL(inputImg01.files[0]);
+}
+inputImg02.onchange = function(){
+    image02.style.width = maxwidth02+'px';
+    image02.style.height = maxheight02+'px';
+    image02.src = URL.createObjectURL(inputImg02.files[0]);
+}
 
 // Method to change the drop title text
 title.addEventListener("input", function(){
@@ -58,5 +125,10 @@ borderThickness.addEventListener("input", function(){
 // Method to change the border color of the title
 borderColor.addEventListener("input", function(){
     editable.style.setProperty('--font_border_color', borderColor.value);
+});
+
+// Method to submit changes to server
+submit.addEventListener("click", function(){
+    console.log("submitted");
 });
 
