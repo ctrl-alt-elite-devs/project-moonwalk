b6c64795cf1e0834060044e31ea1857c4071d5de
ValiantPhoenix13 <axelkh2001@hotmail.com>
2025-03-18 09:06:17 -0700

    Late commit

--- static/scripts/countdown_banner.js
+++ static/scripts/countdown_banner.js
@@ -2,12 +2,12 @@
 // Get the time from python using django
 var remainingTime_str = data;
 var remainingTime = parseFloat(remainingTime_str);
-console.log(remainingTime);
+// console.log(remainingTime);
     
 
 // Define the function that will update the html values
 function updateCountdown(){
-    // console.log("Entered the function");
+    console.log("Entered the function");
     // Get each time units values
     var days = Math.floor(remainingTime / 86400);
     var hours = Math.floor((remainingTime % 86400) / 3600);
@@ -18,16 +18,40 @@ function updateCountdown(){
     // console.log(hours)
         
     // Format each unit
-    //days_str = String.format("%02d", days);
-    //hours_str = String.format("%02d", hours);
+    days_str = String.format("%02d", days);
+    hours_str = String.format("%02d", hours);
     //minutes_str = String.format("%02d", minutes);
     //seconds_str = String.format("%02d", seconds);
+    console.log(hours);
+
 
     // Update the values here
-    document.getElementById("number_Days").innerHTML = days;
-    document.getElementById("number_Hours").innerHTML = hours;
-    document.getElementById("number_Minutes").innerHTML = minutes;
-    document.getElementById("number_Seconds").innerHTML = seconds;
+
+    // Check values for proper formatting "00"
+    if (days < 10){
+        document.getElementById("number_Days").innerHTML = "0" + days;
+    } else {
+        document.getElementById("number_Days").innerHTML = days;
+    }
+
+    if (hours < 10){
+        document.getElementById("number_Hours").innerHTML = hours_str;
+    } else {
+        document.getElementById("number_Hours").innerHTML = hours_str;
+    }
+
+    if (minutes < 10){
+        document.getElementById("number_Minutes").innerHTML = "0" + minutes;
+    } else {
+        document.getElementById("number_Minutes").innerHTML = minutes;
+    }
+
+    if (seconds < 10){
+        document.getElementById("number_Seconds").innerHTML = "0" + seconds;
+    } else {
+        document.getElementById("number_Seconds").innerHTML = seconds;
+    }
+    
 
     // Check to see if no time remains
     if (remainingTime <= 0) {
